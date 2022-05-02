5bc8c760cb3b55a8a7829348b363ea9b0e597354
lsepu <55538804+lsepu@users.noreply.github.com>
2022-05-02 11:36:09 -0500

    ADD: Creation of Store Provider and did some tests with the list of actions

--- to-do-frontend/src/App.jsx
+++ to-do-frontend/src/App.jsx
@@ -1,12 +1,14 @@
-import './App.css'
+import "./App.css";
+import ListOfToDo from "./components/ListOfToDo";
+import StoreProvider from "./components/StoreProvider";
 
 function App() {
-
   return (
-    <div className="App">
-      <h1>Test</h1>
-    </div>
-  )
+    <StoreProvider>
+        <h1>Test</h1>
+        <ListOfToDo />
+    </StoreProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ to-do-frontend/src/components/ListOfToDo.jsx
@@ -0,0 +1,27 @@
+import React, { useContext } from "react";
+import { Store } from "./StoreProvider";
+
+const ListOfToDo = () => {
+  const { state, dispatch } = useContext(Store);
+
+  return (
+    <div>
+      <h1>Actions pending</h1>
+      <ul>
+        {state.listOfNotes.map((note) => {
+          return (
+            <li
+              style={note.done ? { textDecoration: "line-through" } : {}}
+              key={note.id}
+            >
+              {note.title} <br />
+              {note.message} <br />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default ListOfToDo;
